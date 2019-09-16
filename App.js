8ef6db90206461d40291a6d2abcadc2b44567870
bocek <bocek83@seznam.cz>
2019-09-16 14:20:38 +0300

    assignment 3

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-import {Alert, Text, View} from 'react-native';
+import {Alert, Text, View, TextInput, Button} from 'react-native';
+import set from "@babel/runtime/helpers/esm/set";
 
 
 /*
@@ -11,97 +12,62 @@ Every time you change the value, screen is rendered - meaning, new values are se
 And again... take a good look at what is shown on the screen - nothing else but what is returned
 */
 export default class App extends Component {
-  state = {
-    myState: String,
-    yourState: String,
-    myStateChanged: Boolean,
-    yourStateChanged: Boolean,
-  };
-
-  //Constructor is not needed. Values can be set above where only types are set.
-  constructor () {
-    super (); //super must be called first
-    this.state = {
-      myState: 'I am at school',
-      yourState: 'You are on a holiday',
-      herState: 'She is abroad',
-      myStateChanged: false,
-      yourStateChanged: false,
-      herStateChanged: false,
+    state = {
+        enteredNumberOne: Number,
+        enteredNumberTwo: Number,
+        result: Number | undefined,
     };
-    //function needs to be bound if written in 'normal' way AND using state 
-    // - actually because we are using "this" -object. this object is used in the method meaning this App object.
-    //this.updateYourState = this.updateYourState.bind (this);
-    //Arrow-type function is newer way and does not need a bind - object 'this' is already bound.
-    // this.updateMyState = this.updateMyState.bind (this);
-  }
 
+    //Constructor is not needed. Values can be set above where only types are set.
+    constructor() {
+        super(); //super must be called first
+        this.state = {};
+    }
 
-  /*
-Function written in arrow format - there is no need to bind. 
-Compare to method updateYourState
-*/
-  updateMyState = () => {
-    const {myStateChanged} = this.state;
-    if (myStateChanged) {
-      this.setState ({
-        myState: 'I am at school',
-        myStateChanged: false,
-      });
-    } else {
-      this.setState ({
-        myState: 'I am at home',
-        myStateChanged: true,
-      });
-    }
-  };
-  updateYourState = () => {
-    if (this.state.yourStateChanged) {
-      this.setState ({
-        //refers to class's state
-        yourState: 'You are on a holiday',
-        yourStateChanged: false,
-      });
-    } else {
-      this.setState ({
-        //refers to class's state
-        yourState: 'You are at work',
-        yourStateChanged: true,
-      });
-    }
-  };
-  updateHerState = () => {
-    if (this.state.herStateChanged) {
-      this.setState ({
-        //refers to class's state
-        herState: 'She is abroad',
-        herStateChanged: false,
-      });
-    } else {
-      this.setState ({
-        //refers to class's state
-        herState: 'She is shopping',
-        herStateChanged: true,
-      });
-    }
-  };
-  /*
-This is what we see - the render part
-In Text elements there are method calls which happen when the element is clicked
-*/
+    calculate = (operation) => {
+        let result = 0;
+        switch (operation) {
+            case '+':
+                result = this.state.enteredNumberOne + this.state.enteredNumberTwo;
+                break;
+            case '-':
+                result = this.state.enteredNumberOne - this.state.enteredNumberTwo;
+                break;
+            case '*':
+                result = this.state.enteredNumberOne * this.state.enteredNumberTwo;
+                break;
+            case '/':
+                result = this.state.enteredNumberOne / this.state.enteredNumberTwo;
+                break;
+        }
+        console.log(this.state.enteredNumberOne);
+        console.log(this.state.enteredNumberOne + ' ' + operation + ' ' + this.state.enteredNumberTwo + ' = ' + result);
+        this.setState({
+            result: result,
+        })
+    };
 
-  render () {
-    return (
-        <View>
-          <Text onPress={this.updateMyState}> {this.state.myState} </Text>
-          <Text onPress={this.updateYourState}> {this.state.yourState} </Text>
-          <Text onPress={this.updateHerState}> {this.state.herState} </Text>
-        </View>
-    );
-    /*
-    If we did not use bind, the last function call could be written
-    <Text onPress={()=>this.updateYourState()}> {this.state.yourState} </Text>
-    And this also makes the bind - gives the this-object to the method
-    */
-  }
+    render() {
+        return (
+            <View>
+                <TextInput placeholder={"First number"} keyboardType={"numeric"}
+                           onChangeText={(text) => this.setState({enteredNumberOne: Number(text)})}/>
+                <TextInput placeholder={"Second number"} keyboardType={"numeric"}
+                           onChangeText={(text) => this.setState({enteredNumberTwo: Number(text)})}/>
+
+                <Button title="+" onPress={() => this.calculate('+')}/>
+                <Button title="-" onPress={() => this.calculate('-')}/>
+                <Button title="*" onPress={() => this.calculate('*')}/>
+                <Button title="/" onPress={() => this.calculate('/')}/>
+
+                <TextInput placeholder={"Result"} readonly={true}
+                           value={this.state.result && this.state.result + ''}/>
+            </View>
+        );
+        /*
+        If we did not use bind, the last function call could be written
+        <Text onPress={()=>this.updateYourState()}> {this.state.yourState} </Text>
+        And this also makes the bind - gives the this-object to the method
+        */
+    }
 }
